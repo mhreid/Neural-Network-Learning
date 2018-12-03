18f9ef45a2a05692db6b930be73c46164ed59bf6
mhreid <micahhelenreid@gmail.com>
2018-12-03 15:52:50 -0500

    Efficiency and reformatting

--- main.py
+++ main.py
@@ -8,63 +8,81 @@ training = pd.read_csv('mnist_train.csv')
 #This confirms that the data is there
 
 
-def make_vector(index):
-    num = training.loc[index, :][0]
-    vector = training.loc[index,:][1:]
-    return [num, vector]
-
 def make_traindata():
-    data = []
-    for i in range(training.shape[0]):
-        data.append(make_vector(i))
-        #each vector is actually of format [number, [vector]]
-    return data
+    nums = training.iloc[:,0]
+    nums = nums.as_matrix()
+    data = training.iloc[:,1:]
+    data = data.as_matrix()
+    return nums,data
 
 def make_rand_layer(input, output):
-    layer = []
+    """layer = []
     for i in range(input):
         row = []
         for j in range(output):
             row.append(random.random())
-        layer.append(row)
+        layer.append(row)"""
+    layer = np.random.rand(input, output) * .01
+
     return layer
 
 def make_layers(input_size, output_size, num):
-    #hidden layer neurons are difference in input and output, (input must be greater size)
-    hidden_size = input_size - output_size
     hidden_size = 20
-    layers = [make_rand_layer(input_size, hidden_size)]
-    for i in range(num - 1):
-        layers.append(make_rand_layer(hidden_size, hidden_size))
-    layers.append(make_rand_layer(hidden_size, output_size))
+    layers = []
+    if(num <= 1):
+        layers.append(make_rand_layer(input_size, output_size))
+    else:
+        layers.append(make_rand_layer(input_size, hidden_size))
+        for i in range(num - 2):
+            layers.append(make_rand_layer(hidden_size, hidden_size))
+        layers.append(make_rand_layer(hidden_size, output_size))
     return layers
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
-def vector_sigmoid(vector):
-    out = []
-    for x in vector:
-        out.append(sigmoid(x))
-    return out
+def sigmoid_all(x):
+    sig = np.array([[sigmoid(i) for i in j] for j in x])
+    return sig
+
 
 def forward(layers, data):
-    out = []
+    a = []
+    z = []
+    a.append(data.T)
+    for layer in layers:
+        print("layer")
+        z.append(np.dot(a[-1], layer))
+        a.append(sigmoid_all(z[-1]))
+    return a[1:], z
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+def w_sum(num):
+    sum = 0
+    for i in range(len(num[1])):
+        print("layer " + str(i))
+        if(num[0] == i - 1):
+            sum += (1 - num[1][i]) ** 2
+        else:
+            sum += num[1][i] ** 2
+    return sum
+
+def w_sum_all(data):
+    sum = 0
     for d in data:
-        x = d[1]
-        for layer in layers:
-            x = np.dot(x, layer)
-            x = vector_sigmoid(x)
-        out.append([d[0], x])
-    return out
+        sum += w_sum(d)
+    print(sum)
+    sum /= len(data)
+    print(len(data))
+    return sum
+
 
 if __name__ == '__main__':
-    data = make_traindata()
-    data = data
-    print("data made")
-    size = len(data[0][1])
-    layers = make_layers(size,10,1)
-    print("layers made")
-    out = forward(layers, data)
-    print("done!!")
-    print(out[0][1])
+    nums, data = make_traindata()
+    print(data.shape[0])
+    layers = make_layers(data.shape[0], 10, 3)
+    print(len(layers))
+    a,z = forward(layers, data)
+    print(a[-1][0])
