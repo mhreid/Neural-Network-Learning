1aa306808f7edaa8a038d83ce9d457bda15579bd
mhreid <micahhelenreid@gmail.com>
2018-12-09 21:07:11 -0500

    Works well with two samples and biases

--- main_biases.py
+++ main_biases.py
@@ -18,7 +18,7 @@ def make_traindata():
 
 def make_rand_layer(input, output):
     #tested and works
-    layer = (np.random.rand(input, output) - .5) * 2
+    layer = (np.random.rand(input, output) - .5)
     return layer
 
 def make_layers(input_size, output_size, num):
@@ -26,6 +26,7 @@ def make_layers(input_size, output_size, num):
     #should have a minimum of two layers for num, which is equivalent to 1 hidden layer
     hidden_size = 100
     layers = []
+    biases = []
     if(num <= 1):
         layers.append(make_rand_layer(input_size, output_size))
     else:
@@ -33,7 +34,10 @@ def make_layers(input_size, output_size, num):
         for i in range(num - 2):
             layers.append(make_rand_layer(hidden_size, hidden_size))
         layers.append(make_rand_layer(hidden_size, output_size))
-    return layers
+    for i in range(num - 1):
+        biases.append(np.random.rand(1, hidden_size))
+    biases.append(np.random.rand(1, output_size))
+    return layers, biases
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -44,12 +48,12 @@ def sigmoid_all(x, func):
     return sig
 
 
-def forward(layers, data):
+def forward(layers, biases, data):
     #tested and is giving same answer regardless of input unless weights are super super small
     a = [data]
     z = []
-    for layer in layers:
-        z.append(np.dot(a[-1], layer))
+    for i in range(len(layers)):
+        z.append(np.dot(a[-1], layers[i]) + biases[i])
         a.append(sigmoid_all(z[-1], sigmoid))
     return a, z
 
@@ -69,7 +73,7 @@ def d_loss_sum_all(nums, y_hats):
         sum.append(d_loss_sum(nums[i], y_hats[:][i]))
     return np.matrix(sum)
 
-def gradients(nums, a, z, layers):
+def gradients(nums, a, z, layers, biases):
     #a 0,1,2 z 1,2, l 1,2
     #their activation numbers match mine
     #their weights also match mine
@@ -79,34 +83,44 @@ def gradients(nums, a, z, layers):
     #in progress, need to work on transposes being correct
 
     d_layers = []
+    d_biases = []
     d_cost = [np.multiply(-d_loss_sum_all(nums, a[-1]), sigmoid_all(z[-1], sigmoid_derivative))]
     d_layers.append(np.dot(a[-2].T,d_cost[-1]))
+    d_biases.append(np.sum(d_cost[-1]))
     for i in range(len(layers) - 2, -1, -1):
         d_cost.append(np.multiply(np.dot(d_cost[-1], layers[i + 1].T), sigmoid_all(z[i], sigmoid_derivative)))
-
-
+        d_biases.append(np.sum(d_cost[-1]))
         d_layers.append(np.dot(a[i].T, d_cost[-1]))
     d_layers.reverse()
-    return d_layers
+    d_biases.reverse()
+    return d_layers, d_biases
 
-def back_prop(layers, data, nums, step, runs):
+def back_prop(layers, biases, data, nums, step, runs):
     for j in range(runs):
-        a,z = forward(layers, data)
+        a,z = forward(layers, biases, data)
+        output = a[-1]
+        print(output[1])
+        print(nums[1])
+        print(output[0])
+        print(nums[0])
         print("backpropagation " + str(j + 1) + " out of " + str(runs))
-        d_layers = gradients(nums, a, z, layers)
+        d_layers, d_biases = gradients(nums, a, z, layers, biases)
         for i in range(len(layers)):
             layers[i] -= np.asarray(d_layers[i]) * step
+            biases[i] -= d_biases[i] * step
 
-    a,z = forward(layers, data)
+    a,z = forward(layers, biases, data)
     return layers, a[-1]
 
 
 if __name__ == '__main__':
     nums, data = make_traindata()
-    nums = nums[1:3]
-    data = data[1:3]
-    layers = make_layers(data.shape[1], 10, 2)
-    layers, output = back_prop(layers, data, nums, .1, 10)
+    nums = nums[:2]
+    data = data[:2]
+    layers, biases = make_layers(data.shape[1], 10, 2)
+    layers, output = back_prop(layers, biases, data, nums, .1, 100)
+    #a, z = forward(layers, biases, data)
+    #output = a[-1]
     print(output[1])
     print(nums[1])
     print(output[0])
